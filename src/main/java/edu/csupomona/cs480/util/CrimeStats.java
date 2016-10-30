a590b57d91ebf4d9d5591adb8a9f10edbcf34c5b
Lucodivo <ConnorAHaskins@gmail.com>
2016-10-29 17:49:43 -0700

    fixed the broken indentation

--- src/main/java/edu/csupomona/cs480/util/CrimeStats.java
+++ src/main/java/edu/csupomona/cs480/util/CrimeStats.java
@@ -4,60 +4,58 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
- * 
  * @author Jonathan T. Fetzer
  * clean-up/documentation: Connor A. Haskins
  * class: CS 480, Cal Poly Pomona
  * group: Sounds Neat
- *
  */
 
 /**
  * Class used to hold all of the crime statistics of a city
  */
-public class CrimeStats {	
+public class CrimeStats {
 
-	private String city;
-	private String state;
-	
-	private int amISafeIndex; // takes on values 1 through 5 with 3 being the baseline (e.g. state or national average).
-	
-	private int[] crimeDataYears;
-	private int[] numMurders;
-	private int[] numRapes;
-	private int[] numRobberies;
-	private int[] numAssaults;
-	private int[] numBurglaries;
-	private int[] numThefts;
-	private int[] numAutoThefts;
-	private int[] numArsons;
-	
-	private float[] murderStats;
-	private float[] rapeStats;
-	private float[] robberyStats;
-	private float[] assaultStats;
-	private float[] burglaryStats;
-	private float[] theftStats;
-	private float[] autoTheftStats;
-	private float[] arsonStats;
-	private float[] crimeDataIndex;
+    private String city;
+    private String state;
+
+    private int amISafeIndex; // takes on values 1 through 5 with 3 being the baseline (e.g. state or national average).
+
+    private int[] crimeDataYears;
+    private int[] numMurders;
+    private int[] numRapes;
+    private int[] numRobberies;
+    private int[] numAssaults;
+    private int[] numBurglaries;
+    private int[] numThefts;
+    private int[] numAutoThefts;
+    private int[] numArsons;
+
+    private float[] murderStats;
+    private float[] rapeStats;
+    private float[] robberyStats;
+    private float[] assaultStats;
+    private float[] burglaryStats;
+    private float[] theftStats;
+    private float[] autoTheftStats;
+    private float[] arsonStats;
+    private float[] crimeDataIndex;
 
     /**
      * Create an instance of CrimeState using city and state name strings
      * @param city
      * @param state
      */
-	CrimeStats(String city, String state){
-		this.city = city;
-		this.state = state;
-	} // end constructor CrimeStats
+    CrimeStats(String city, String state) {
+        this.city = city;
+        this.state = state;
+    } // end constructor CrimeStats
 
     /**
      * Getters
-      */
+     */
 
-    public int getCrimeDataYear(int index){
-        if(index < 0 || index > crimeDataYears.length){
+    public int getCrimeDataYear(int index) {
+        if (index < 0 || index > crimeDataYears.length) {
             throw new IndexOutOfBoundsException("index passed: " + index +
                     ", must be between 0 and " + crimeDataYears.length +
                     " for " + city + ", " + state);
@@ -65,11 +63,11 @@ public class CrimeStats {
         return crimeDataYears[index];
     }
 
-    public String getCity(){
+    public String getCity() {
         return city;
     }
 
-    public String getState(){
+    public String getState() {
         return state;
     }
 
@@ -229,90 +227,90 @@ public class CrimeStats {
         // TODO: calculate amISafeIndex using crimeDataIndex
     }
 
-    public void setCity(String city){
+    public void setCity(String city) {
         this.city = city;
     }
 
-    public void setState(String state){
+    public void setState(String state) {
         this.state = state;
     }
 
-	public void setNumArsons(int[] numArsons) {
-		this.numArsons = numArsons;
-	}
+    public void setNumArsons(int[] numArsons) {
+        this.numArsons = numArsons;
+    }
 
-	public void setNumAutoThefts(int[] numAutoThefts) {
-		this.numAutoThefts = numAutoThefts;
-	}
+    public void setNumAutoThefts(int[] numAutoThefts) {
+        this.numAutoThefts = numAutoThefts;
+    }
 
-	public void setNumThefts(int[] numThefts) {
-		this.numThefts = numThefts;
-	}
+    public void setNumThefts(int[] numThefts) {
+        this.numThefts = numThefts;
+    }
 
-	public void setNumBurglaries(int[] numBurgleries) {
-		this.numBurglaries = numBurgleries;
-	}
+    public void setNumBurglaries(int[] numBurgleries) {
+        this.numBurglaries = numBurgleries;
+    }
 
-	public void setNumAssaults(int[] numAssaults) {
-		this.numAssaults = numAssaults;
-	}
+    public void setNumAssaults(int[] numAssaults) {
+        this.numAssaults = numAssaults;
+    }
 
-	public void setCrimeDataYears(int[] crimeDataYears){
-		this.crimeDataYears = crimeDataYears;
-	} // end method setCrimeDataYears
+    public void setCrimeDataYears(int[] crimeDataYears) {
+        this.crimeDataYears = crimeDataYears;
+    } // end method setCrimeDataYears
 
-	public void setMurderStats(float[] murderStats) {
-		this.murderStats = murderStats;
-	}
+    public void setMurderStats(float[] murderStats) {
+        this.murderStats = murderStats;
+    }
 
-	public void setRapeStats(float[] rapeStats) {
-		this.rapeStats = rapeStats;
-	}
+    public void setRapeStats(float[] rapeStats) {
+        this.rapeStats = rapeStats;
+    }
 
-	public void setRobberyStats(float[] robberyStats) {
-		this.robberyStats = robberyStats;
-	}
+    public void setRobberyStats(float[] robberyStats) {
+        this.robberyStats = robberyStats;
+    }
 
-	public void setAssaultStats(float[] assaultStats) {
-		this.assaultStats = assaultStats;
-	}
+    public void setAssaultStats(float[] assaultStats) {
+        this.assaultStats = assaultStats;
+    }
 
-	public void setBurglaryStats(float[] burglaryStats) {
-		this.burglaryStats = burglaryStats;
-	}
+    public void setBurglaryStats(float[] burglaryStats) {
+        this.burglaryStats = burglaryStats;
+    }
 
-	public void setTheftStats(float[] theftStats) {
-		this.theftStats = theftStats;
-	}
+    public void setTheftStats(float[] theftStats) {
+        this.theftStats = theftStats;
+    }
 
-	public void setAutoTheftStats(float[] autoTheftStats) {
-		this.autoTheftStats = autoTheftStats;
-	}
+    public void setAutoTheftStats(float[] autoTheftStats) {
+        this.autoTheftStats = autoTheftStats;
+    }
 
-	public void setArsonStats(float[] arsonStats) {
-		this.arsonStats = arsonStats;
-	}
+    public void setArsonStats(float[] arsonStats) {
+        this.arsonStats = arsonStats;
+    }
 
-	public void setNumMurders(int[] numMurders) {
-		this.numMurders = numMurders;
-	}
+    public void setNumMurders(int[] numMurders) {
+        this.numMurders = numMurders;
+    }
 
-	public void setNumRapes(int[] numRapes) {
-		this.numRapes = numRapes;
-	}
+    public void setNumRapes(int[] numRapes) {
+        this.numRapes = numRapes;
+    }
 
-	public void setNumRobberies(int[] numRobberies) {
-		this.numRobberies = numRobberies;
-	}
+    public void setNumRobberies(int[] numRobberies) {
+        this.numRobberies = numRobberies;
+    }
 
     /**
      * Helper function to link the desired year to the proper index of the arrays
      * @param year (ex: 2011)
      * @return an index (ex: 2, to be used for numRobberries[2])
      */
-    private int getYearIndex(int year){
+    private int getYearIndex(int year) {
         for (int i = 0; i < crimeDataYears.length; i++) {
-            if(crimeDataYears[i] == year){
+            if (crimeDataYears[i] == year) {
                 return i;
             }
         }
@@ -323,7 +321,7 @@ public class CrimeStats {
      * Super cool function that turns the CrimeStats object into JSON format
      * @return
      */
-	public String toJson() {
+    public String toJson() {
         ObjectMapper mapper = new ObjectMapper();
         String jsonString = null;
         try {
