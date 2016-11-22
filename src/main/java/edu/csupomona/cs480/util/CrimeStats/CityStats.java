1c61ea0581ff0d961c9a16b3f2691ce7468cd6af
Jonathan T. Fetzer <jonathantfetzer@gmail.com>
2016-11-22 00:23:17 -0800

    Update CityStats.java

--- src/main/java/edu/csupomona/cs480/util/CrimeStats/CityStats.java
+++ src/main/java/edu/csupomona/cs480/util/CrimeStats/CityStats.java
@@ -141,13 +141,13 @@ public class CityStats {
         // average out the index
         index /= crimeDataIndex.length;
         if (index > 50000) { // safe
-            amISafeIndex = 1;
+            amISafeIndex = 5;
         } else if (index > 36000) { // less safe
-            amISafeIndex = 1;
+            amISafeIndex = 4;
         } else if (index > 25000) { // average
-            amISafeIndex = 1;
+            amISafeIndex = 3;
         } else if (index > 18000) { // dangerous
-            amISafeIndex = 1;
+            amISafeIndex = 2;
         } else { // very dangerous
             amISafeIndex = 1;
         }
